9ba1a6f5995a7022e42cf04b75b164a2920f15d4
Pedro-Benevides <pvsbenevides197@gmail.com>
2024-10-25 23:07:36 -0300

    fix(start.sh): ajusta parametros do celery para evitar perda de heartbeats com rabbitmq ao iniciar task chain

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-celery -A plasticome.config.celery worker -l info --pool=solo &
+celery -A plasticome.config.celery worker -l info --pool=processes --autoscale=10,3 --concurrency=2 &
 dockerd --tls=false --host=tcp://0.0.0.0:2375 --host=unix:///var/run/docker.sock &
 cd /app && flask run -h backend
 # celery -A plasticome.config.celery worker -l info
